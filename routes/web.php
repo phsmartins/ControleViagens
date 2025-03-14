63e6149fbab70dc89c060040808af6e61de91574
phsmartins <phsmartins2004@gmail.com>
2025-03-14 09:42:48 -0400

    trips route

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\DriverController;
+use App\Http\Controllers\TripController;
 use App\Http\Controllers\VehicleController;
 use Illuminate\Support\Facades\Route;
 
@@ -10,3 +11,11 @@ Route::get('/', function () {
 
 Route::resource('vehicles', VehicleController::class);
 Route::resource('drivers', DriverController::class);
+
+Route::resource('trips', TripController::class);
+Route::get('/trips/{trip}/finish', [TripController::class, 'finishForm'])->name('trips.finishForm');
+Route::post('/trips/{trip}/finish', [TripController::class, 'finish'])->name('trips.finish');
+Route::get('/trips/{trip}/edit-ongoing', [TripController::class, 'editOngoing'])->name('trips.edit-ongoing');
+Route::put('/trips/{trip}/edit-ongoing', [TripController::class, 'updateOngoing'])->name('trips.update-ongoing');
+Route::get('/trips/{trip}/edit-completed', [TripController::class, 'editCompleted'])->name('trips.edit-completed');
+Route::put('/trips/{trip}/edit-completed', [TripController::class, 'updateCompleted'])->name('trips.update-completed');
