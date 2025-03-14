7d84a9c17091c63bf2b46b051bd3f1616e332757
phsmartins <phsmartins2004@gmail.com>
2025-03-13 23:51:11 -0400

    driver controller

---
+++ app/Http/Controllers/DriverController.php
@@ -0,0 +1,93 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Driver;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Http\Request;
+use Illuminate\View\View;
+
+class DriverController extends Controller
+{
+    public function index(): View
+    {
+        $drivers = Driver::all();
+        return view('drivers.index', compact('drivers'));
+    }
+
+    public function create(): View
+    {
+        return view('drivers.create');
+    }
+
+    public function store(Request $request): RedirectResponse
+    {
+        $validated = $request->validate(
+            [
+                'name' => 'required|string|max:255',
+                'birth_date' => 'required|date|before_or_equal:' . now()->subYears(18)->format('Y-m-d'),
+                'cnh' => 'required|string|unique:drivers,cnh|max:20',
+            ],
+            [
+                'name.required' => 'Nome é obrigatório',
+
+                'birth_date.required' => 'Data de nascimento é obrigatório',
+                'birth_date.before_or_equal' => 'O motorista deve ter 18 ou mais de idade',
+
+                'cnh.required' => 'CNH é obrigatório',
+                'cnh.unique' => 'Este número de CNH já foi cadastrado no sistema',
+            ]
+        );
+
+        Driver::create($validated);
+
+        return redirect()
+            ->route('drivers.index')
+            ->with('success', 'Motorista criado com sucesso');
+    }
+
+    public function show(int $id): View
+    {
+        $driver = Driver::findOrFail($id);
+        return view('drivers.show', compact('driver'));
+    }
+
+    public function edit(Driver $driver): View
+    {
+        return view('drivers.edit', compact('driver'));
+    }
+
+    public function update(Request $request, Driver $driver): RedirectResponse
+    {
+        $validated = $request->validate(
+            [
+                'name' => 'required|string|max:255',
+                'birth_date' => 'required|date|before_or_equal:' . now()->subYears(18)->format('Y-m-d'),
+                'cnh' => 'required|string|unique:drivers,cnh,' . $driver->id . '|max:20',
+            ],
+            [
+                'name.required' => 'Nome é obrigatório',
+
+                'birth_date.required' => 'Data de nascimento é obrigatório',
+                'birth_date.before_or_equal' => 'O motorista deve ter 18 ou mais de idade',
+
+                'cnh.required' => 'CNH é obrigatório',
+                'cnh.unique' => 'Este número de CNH já foi cadastrado no sistema',
+            ]
+        );
+
+        $driver->update($validated);
+
+        return redirect()
+            ->route('drivers.index')
+            ->with('success', 'Motorista atualizado com sucesso');
+    }
+
+    public function destroy(Driver $driver): RedirectResponse
+    {
+        $driver->delete();
+        return redirect()
+            ->route('drivers.index')
+            ->with('success', 'Motorista deletado com sucesso');
+    }
+}
